c6c21b371f0505c0483bbc7dcebc868bd34a87db
7alip <talipaltas@gmail.com>
2025-02-24 12:25:27 +0100

    Add email

--- apps/hvn/src/payload.config.ts
+++ apps/hvn/src/payload.config.ts
@@ -1,7 +1,10 @@
 // storage-adapter-import-placeholder
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { nodemailerAdapter } from '@payloadcms/email-nodemailer'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
+import { formBuilderPlugin } from '@payloadcms/plugin-form-builder'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import nodemailer from 'nodemailer'
 import path from 'path'
 import { buildConfig } from 'payload'
 import sharp from 'sharp'
@@ -25,8 +28,34 @@ export default buildConfig({
     url: process.env.DATABASE_URL as string,
   }),
   editor: lexicalEditor(),
+  email: nodemailerAdapter({
+    defaultFromAddress: 'info@harmonievannederland.com',
+    defaultFromName: 'Harmonie van Nederland',
+    // Any Nodemailer transport
+    transport: await nodemailer.createTransport({
+      auth: {
+        pass: process.env.EMAIL_PASS,
+        user: 'info@harmonievannederland.com',
+      },
+      host: 'mail.privateemail.com',
+      port: 465,
+    }),
+  }),
   plugins: [
     payloadCloudPlugin(),
+    formBuilderPlugin({
+      fields: {
+        checkbox: true,
+        country: true,
+        email: true,
+        message: true,
+        number: true,
+        select: true,
+        state: false,
+        text: true,
+        textarea: true,
+      },
+    }),
     // storage-adapter-placeholder
   ],
   secret: process.env.PAYLOAD_SECRET || '',
