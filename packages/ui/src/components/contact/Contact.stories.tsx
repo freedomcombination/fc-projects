fc0043997e079a24aee4b801ab0dc82b1b92cafc
Mustafa Budak <mbudak@gmail.com>
2025-02-27 10:05:09 +0100

    this story args are empty. You could fill this part.

--- packages/ui/src/components/contact/Contact.stories.tsx
+++ packages/ui/src/components/contact/Contact.stories.tsx
@@ -13,9 +13,9 @@ type Story = StoryObj<typeof Contact>
 export const Default: Story = {
   // TODO: Fill args
   args: {
-    address: '',
-    email: '',
-    phone: '',
-    title: '',
+    address: "Merkez Mah. Destek Cad. No:1, İstanbul",
+    email: "info@lotusvdmedia.nl",
+    phone: "+90 (212) 555 0000",
+    title: "Bize Ulaşın",
   },
 }
