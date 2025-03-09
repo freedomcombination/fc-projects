dd15d422de78e71c87b1ea34ccf67a4b88f94d39
ayseugurlu <ayseugurlu9135@gmail.com>
2025-03-09 17:30:04 +0100

    feat:added herosection for support page

---
+++ apps/lotus/app/[locale]/support/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import SupportHeroComponent from '@fc/ui/components/support/SupportHeroComponent'
+
+const page = () => {
+  return (
+    <div>
+      <SupportHeroComponent />
+    </div>
+  )
+}
+
+export default page
