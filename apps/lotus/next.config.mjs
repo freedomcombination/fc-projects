08ce968bd88c87fc298fd1c4c406c856629169c7
7alip <talipaltas@gmail.com>
2025-03-12 00:15:53 +0100

    Update dockers

--- apps/lotus/next.config.mjs
+++ apps/lotus/next.config.mjs
@@ -1,13 +1,15 @@
 import { getNextConfig } from '@fc/intl/next.config'
 
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const nextConfig = getNextConfig({
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../../'),
   },
   output: 'standalone',
-  transpilePackages: ['@fc/ui'],
 })
 
 export default nextConfig
