37e8466712c1e67d8939c3140811f3504bb79561
7alip <talipaltas@gmail.com>
2025-03-12 18:15:01 +0100

    Fix importing Hero

--- apps/lotus/app/[locale]/support/page.tsx
+++ apps/lotus/app/[locale]/support/page.tsx
@@ -1,21 +1,18 @@
 import React from 'react'
 
-import Hero from '@fc/ui/components/Hero/Hero'
+import { Hero } from '@fc/ui/components/Hero/Hero'
 
 const heroInfo = {
   image: 'https://sarkac.org/wp-content/uploads/2023/08/dayanisma.png',
-  subtitle: 'Topluluğumuzu güçlendirmek ve daha iyi bir gelecek inşa etmek için birlikte çalışalım. Her destek, yarınlarımız için umut olur.',
+  subtitle:
+    'Topluluğumuzu güçlendirmek ve daha iyi bir gelecek inşa etmek için birlikte çalışalım. Her destek, yarınlarımız için umut olur.',
   title: 'Destek Ver, Fark Yarat',
 }
 
 const page = () => {
   return (
     <div>
-      <Hero 
-        backgroundImage={heroInfo.image}
-        subtitle={heroInfo.subtitle}
-        title= {heroInfo.title}
-      />
+      <Hero backgroundImage={heroInfo.image} subtitle={heroInfo.subtitle} title={heroInfo.title} />
     </div>
   )
 }
