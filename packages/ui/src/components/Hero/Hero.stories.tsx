a3e5bff8d3e4fa50e1864d3f6ebbdc57e13423f2
7alip <talipaltas@gmail.com>
2025-03-12 13:22:21 +0100

    Update hero styles

--- packages/ui/src/components/Hero/Hero.stories.tsx
+++ packages/ui/src/components/Hero/Hero.stories.tsx
@@ -1,8 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import Hero from './Hero'
+import { Hero } from './Hero'
 
 const meta: Meta<typeof Hero> = {
+  args: {
+    backgroundImage: 'https://sarkac.org/wp-content/uploads/2023/08/dayanisma.png',
+    subtitle:
+      'Topluluğumuzu güçlendirmek ve daha iyi bir gelecek inşa etmek için birlikte çalışalım. Her destek, yarınlarımız için umut olur.',
+    title: 'Destek Ver, Fark Yarat',
+  },
   component: Hero,
 }
 
@@ -10,11 +16,10 @@ export default meta
 
 type Story = StoryObj<typeof Hero>
 
-export const Default: Story = {
+export const Default: Story = {}
+
+export const HalfHeight: Story = {
   args: {
-    image: 'https://sarkac.org/wp-content/uploads/2023/08/dayanisma.png',
-    subtitle:
-      'Topluluğumuzu güçlendirmek ve daha iyi bir gelecek inşa etmek için birlikte çalışalım. Her destek, yarınlarımız için umut olur.',
-    title: 'Destek Ver, Fark Yarat',
+    className: 'h-[50vh]',
   },
 }
--- packages/ui/src/components/Hero/Hero.tsx
+++ packages/ui/src/components/Hero/Hero.tsx
@@ -1,18 +1,21 @@
+import { cn } from '@fc/ui/lib/utils'
+
 import Image from 'next/image'
 
 export interface HeroProps {
-  title?: string
+  title: string
   subtitle?: string
-  backgroundImage?: string
+  backgroundImage: string
+  className?: string
 }
 
-const Hero = ({ backgroundImage, subtitle, title }: HeroProps) => {
+export const Hero = ({ backgroundImage, className, subtitle, title }: HeroProps) => {
   return (
-    <div className="relative h-screen overflow-hidden flex justify-center items-center">
+    <div className={cn('relative h-screen flex justify-center items-center', className)}>
       <div className="absolute inset-0">
         <Image
           alt="Background"
-          className="brightness-75"
+          className="brightness-40"
           layout="fill"
           objectFit="cover"
           objectPosition="bottom"
@@ -20,12 +23,10 @@ const Hero = ({ backgroundImage, subtitle, title }: HeroProps) => {
         />
       </div>
 
-      <div className="relative h-full w-full flex flex-col items-center justify-center text-center px-4 md:px-8 z-10 bg-[var(--foreground)]/30 rounded-sm">
+      <div className="relative container flex flex-col items-center justify-center text-center px-4 md:px-8">
         <h1 className="text-3xl md:text-4xl font-bold text-white mb-4">{title}</h1>
         <p className="text-sm md:text-base text-white max-w-xl">{subtitle}</p>
       </div>
     </div>
   )
 }
-
-export default Hero
