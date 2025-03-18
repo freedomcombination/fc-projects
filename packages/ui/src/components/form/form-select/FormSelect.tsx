85e68299f371fdf93a423945a058cc30140b23f0
7alip <talipaltas@gmail.com>
2025-03-18 01:21:38 +0100

    Fix FormSelect

--- packages/ui/src/components/form/form-select/FormSelect.tsx
+++ packages/ui/src/components/form/form-select/FormSelect.tsx
@@ -1,12 +1,18 @@
 'use client'
 
-import React, { useRef, useState } from 'react'
+import React from 'react'
 import type { ControllerProps, FieldPath, FieldValues } from 'react-hook-form'
 import { useFormContext } from 'react-hook-form'
 
+import { Button } from '@fc/ui/base/button'
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@fc/ui/base/command'
 import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@fc/ui/base/form'
-import { Input } from '@fc/ui/base/input'
-import { Select as SelectComponent, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@fc/ui/base/select'
+import { Popover, PopoverContent, PopoverTrigger } from '@fc/ui/base/popover'
+import { Select as SelectComponent } from '@fc/ui/base/select'
+import { cn } from '@fc/ui/lib/utils'
+
+import { Check, ChevronsUpDown } from 'lucide-react'
+import { useTranslations } from 'next-intl'
 
 type Option = {
   label: string | null
@@ -19,6 +25,7 @@ type FormSelectProps<T extends FieldValues = FieldValues, N extends FieldPath<T>
   label?: string | null
   name: N
   options: Option[]
+  placeholder?: string
   required?: boolean
   disabled?: boolean
 } & Pick<ControllerProps<T, N>, 'disabled'> &
@@ -33,22 +40,7 @@ export const FormSelect = <T extends FieldValues = FieldValues, N extends FieldP
   required,
 }: FormSelectProps<T, N>) => {
   const form = useFormContext<T, N>()
-  const [searchTerm, setSearchTerm] = useState('')
-
-  const inputRef = useRef<HTMLInputElement>(null)
-
-  const filteredOptions = searchTerm
-    ? options.filter((option) => option.label?.toLowerCase().includes(searchTerm.toLowerCase()))
-    : options
-
-  const onOpenChange = (open: boolean) => {
-    if (open) {
-      setTimeout(() => {
-        inputRef.current?.focus()
-      }, 100)
-    }
-  }
-
+  const t = useTranslations()
   return (
     <FormField<T, N>
       control={form.control}
@@ -67,34 +59,44 @@ export const FormSelect = <T extends FieldValues = FieldValues, N extends FieldP
                 )}
               </FormLabel>
             )}
-            <FormControl>
-              <SelectComponent disabled={disabled} onOpenChange={onOpenChange} {...field}>
-                <SelectTrigger className="w-full relative" id={name}>
-                  <SelectValue placeholder={field.value ? field.value : label} />
-                </SelectTrigger>
-
-                <SelectContent>
-                  <Input
-                    className="w-full p-2 mb-2 border rounded-md bg-transparent outline-none focus:ring-2 focus:ring-blue-500"
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    onFocus={(e) => e.stopPropagation()}
-                    placeholder="Search..."
-                    ref={inputRef}
-                    type="text"
-                    value={searchTerm}
-                  />
-                  {filteredOptions.length > 0 ? (
-                    filteredOptions.map(({ label, value }: Option) => (
-                      <SelectItem key={value} value={value}>
-                        {label}
-                      </SelectItem>
-                    ))
-                  ) : (
-                    <div className="p-2 text-gray-500">No results found</div>
-                  )}
-                </SelectContent>
-              </SelectComponent>
-            </FormControl>
+            <Popover>
+              <PopoverTrigger asChild>
+                <FormControl>
+                  <Button
+                    className={cn('w-full justify-between', !field.value && 'text-muted-foreground')}
+                    role="combobox"
+                    variant="outline"
+                  >
+                    {field.value ? options.find((option) => option.value === field.value)?.label : label}
+                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                  </Button>
+                </FormControl>
+              </PopoverTrigger>
+              <PopoverContent className="w-full p-0">
+                <Command>
+                  <CommandInput placeholder={t('search')} />
+                  <CommandList>
+                    <CommandEmpty>No language found.</CommandEmpty>
+                    <CommandGroup>
+                      {options.map((option) => (
+                        <CommandItem
+                          key={option.value}
+                          onSelect={() => {
+                            field.onChange(option.value)
+                          }}
+                          value={option.label ?? ''}
+                        >
+                          {option.label}
+                          <Check
+                            className={cn('ml-auto', option.value === field.value ? 'opacity-100' : 'opacity-0')}
+                          />
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </CommandList>
+                </Command>
+              </PopoverContent>
+            </Popover>
 
             {description && <FormDescription>{description}</FormDescription>}
             <FormMessage />
